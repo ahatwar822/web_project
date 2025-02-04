adfa6fb79a00d859fa3443507d0dab48c4f77d01
ahatwar822 <himanshugaupale875@gmail.com>
2025-02-04 22:35:47 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+function updateTime() {
+    const now = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    document.getElementById('pinkpart').innerText = now.toLocaleDateString('en-US', options);
+}
+setInterval(updateTime, 1000);  // Update every second
+updateTime();  // Initial call to display date and time immediately
+
 var tl = gsap.timeline({
     scrollTrigger: {
         trigger: "#home",
         start: "top top",
         scrub: 1,
         markers: true,
-        pin: true
+        pin: true,
+        end:"bottom -250%"
     }
 })
 
@@ -30,7 +39,7 @@ tl.to("#circle #bottom img", {
         ease: Power1
     }, "hello")
 
-    .to("#cimg h5", {
+    .to("#centerimg h5", {
         opacity: 0,
         stagger: .1,
         ease: Power1
@@ -41,13 +50,13 @@ tl.to("#circle #bottom img", {
         ease: Power1
     }, "hello")
 
-    .to("#overlay>h1", {
-        bottom:"-50",
+    .to("#overlay>h1#gallery", {
+        bottom: "-100%",
         ease: Power1
     }, "hello")
 
     .to("#gola", {
-        top:"50%",
+        top: "50%",
         scale: 2,
         duration: 1,
         ease: Power1
@@ -63,8 +72,26 @@ tl.to("#circle #bottom img", {
         ease: Power1
     }, "hello2")
 
-    
+
     .to("#circle", {
         scale: 0,
         ease: Power1
     }, "hello2")
+
+    .to("#overlay>h1#pf", {
+        rotate: 0,
+        bottom: "2%",
+        ease: Power1
+    }, "hello2")
+
+    .to("#pinkpart", {
+        duration: 5,
+        top:0,
+        ease: Power1
+    }, "hello2")
+
+    .to("#pinkpart", {
+        top:"-30%",
+        duration: 1,
+        ease: Power1
+    })
